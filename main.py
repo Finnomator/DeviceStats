91ad2e9329a1e15c5cfa8828ae3308169563c0ab
Finn <finndruenert@gmail.com>
2024-04-18 22:36:15 +0200

    allow cross origin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 import system_info
 
 app = FastAPI(default_response_class=ORJSONResponse)
@@ -8,8 +10,16 @@ sys_reporter = system_info.SystemInfoReporter()
 sys_reporter.start()
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/sysinfo")
-async def system_status():
+def system_status():
     return sys_reporter.get_data()
 
 
