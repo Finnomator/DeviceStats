c5cc5bd29bcf76d8b389723b117078c918ca3789
Finn <finndruenert@gmail.com>
2024-04-22 16:35:02 +0200

    return pihole info function

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from subprocess import check_output
+from requests import request
 
 import system_info
 import secrets
 
+PIHOLE_API_KEY = check_output("cat /etc/pihole/setupVars.conf | grep -o 'WEBPASSWORD=.*$' |cut -d '=' -f 2-",
+                              shell=True).decode("ascii").strip()
+
 app = FastAPI(default_response_class=ORJSONResponse)
 sys_reporter = system_info.SystemInfoReporter()
 sys_reporter.start()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,6 +27,11 @@ app.add_middleware(
 @app.get("/sysinfo")
 def system_status():
     return sys_reporter.get_data()
+
+
+@app.get("/pihole-info")
+def pihole_info():
+    return request("GET", f"http://localhost:80/admin/api.php?auth={PIHOLE_API_KEY}&summaryRaw")
 
 
 @app.get("/available-pis")
